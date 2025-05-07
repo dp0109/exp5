b79aea0086aa887a5991b792e7028441d71f4238
Sufiyan0007 <sufiyanlaxmeshwar17@gmail.com>
2025-05-07 05:58:28 +0530

    fib

---
+++ fib.java
@@ -0,0 +1,16 @@
+package exp5;
+
+public class fib {
+    public static void main(String[] args) {
+        int n=10;
+        int a=0;
+        int b=1;
+        System.out.println("fibonacci series upto"+n+"terms:");
+        for(int i=1;i<n;++i){
+            System.out.println(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
